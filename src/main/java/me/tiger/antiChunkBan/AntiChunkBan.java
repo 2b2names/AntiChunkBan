5763654a1eeddd5602288440de544c6b849e99d1
2b2t_names <minecraftdrpurple1@outlook.com>
2025-12-31 14:52:46 +0300

    Initial Commit

---
+++ src/main/java/me/tiger/antiChunkBan/AntiChunkBan.java
@@ -0,0 +1,17 @@
+package me.tiger.antiChunkBan;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AntiChunkBan extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
